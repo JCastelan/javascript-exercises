e42ae99301d742385d56d8ce2281b873a0782035
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-09-30 21:49:27 -0700

    completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(p_year) {
+    return (p_year%4 == 0 && (p_year%100 != 0 || (p_year%100 == 0 && p_year%400 == 0)) );
 };
 
 // Do not edit below this line

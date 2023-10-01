a5b3589fb789003d946111273bef913c7c55a5da
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-09-30 21:08:35 -0700

    completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(p_array, ...p_vals) {
+    let index = 0;
+    while( index < p_array.length){
+        if(p_vals.includes(p_array[index])){
+            p_array.splice(index,1);
+        }else{
+            index++;
+        }
+    }
+    return p_array;
 };
 
 // Do not edit below this line

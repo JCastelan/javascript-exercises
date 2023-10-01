61140cf11e760e7c181b23e26b9015c497f81806
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-09-30 21:24:46 -0700

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+    if( firstNum < 0 || secondNum < 0 ||
+        !Number.isInteger(firstNum) || !Number.isInteger(secondNum)) return 'ERROR';
 
+    let largerNum = secondNum;
+    let smallerNum = firstNum;
+    if( firstNum > secondNum ){
+        largerNum = firstNum;
+        smallerNum = secondNum;
+    }
+    let sum = 0;
+    for( ; smallerNum <= largerNum; smallerNum++){
+        sum+=smallerNum;
+    }
+    return sum;
 };
 
 // Do not edit below this line

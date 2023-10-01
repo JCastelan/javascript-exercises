b6f6f41419a9982c59bd59191d4a7c0381d090cf
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-09-30 21:58:53 -0700

    completed exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(p_val) {
+  return Math.round((p_val-32)*(5/9)*10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(p_val) {
+  return Math.round(((p_val*1.8)+32)*10)/10
 };
 
 // Do not edit below this line

d3daada5be84ac784a733799a597986390c3173d
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-10-06 22:42:58 -0700

    completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let currentTime = new Date();
+    let retVal = people.reduce(function(accumulator, item, index, array) {
+        let champAge;
+        let challengerAge;
 
+        if( "yearOfDeath" in accumulator){
+            champAge = accumulator.yearOfDeath-accumulator.yearOfBirth;
+        }else{
+            champAge = currentTime.getFullYear() - accumulator.yearOfBirth;
+        }
+        if( "yearOfDeath" in item){
+            challengerAge = item.yearOfDeath-item.yearOfBirth;
+        }else{
+            challengerAge = currentTime.getFullYear() - item.yearOfBirth;
+        }
+        if(challengerAge > champAge){
+            return item;
+        }
+
+        return accumulator;
+      }, {
+        name:"None", 
+        yearOfBirth:-1,
+        yearOfDeath:-1,
+    });
+    return retVal;
 };
 
 // Do not edit below this line

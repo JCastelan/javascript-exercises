110aca4d775ca36c626524255e2dce8ccc1c6657
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-10-06 22:15:25 -0700

    completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // let retVal=[];
+    // for(let i = 0; i < books.length;i++){
+    //     let book = books[i];
+    //     retVal.push(book.title);
+    // }
+    // return retVal;
 
+    //cooler way:
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line

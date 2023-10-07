3114d6ecff57b1999215e09f040ffc5b381d641f
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-10-06 21:51:36 -0700

    completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(itNum) {
+    itNum = Number(itNum);
+    if(itNum <=0){
+        return "OOPS";
+    }
+    if(itNum <=2){
+        return 1;
+    }
+    let retVal = 1;
+    let prevNum = 1;
+    let temp;
+    for(let i = 0; i < itNum-2; i++){
+        temp = retVal;
+        retVal+=prevNum;
+        prevNum = temp;
+    }
+    return retVal;
 };
 
 // Do not edit below this line

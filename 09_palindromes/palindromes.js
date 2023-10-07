77054b9e59c44afacc08b4798ec30f7fba142def
Joseph Castelan <JCastelan@users.noreply.github.com>
2023-10-06 21:25:40 -0700

    completed exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (aString) {
+    punctRemoved = aString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"").toLowerCase();
+    reversed = punctRemoved.split("").reverse().join("");
+    return punctRemoved == reversed;
 };
 
 // Do not edit below this line
